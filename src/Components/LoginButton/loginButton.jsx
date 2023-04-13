5f23b48dd3a7e2823774da3a82ec1c892b5eaced
achiyat <111647186+achiyat@users.noreply.github.com>
2023-04-13 19:11:44 +0300

    Tic-Tac-Toe5

--- src/Components/LoginButton/loginButton.jsx
+++ src/Components/LoginButton/loginButton.jsx
@@ -8,6 +8,9 @@ export const LoginButton = () => {
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="d-flex flex-column justify-content-center align-items-center">
         <div>
+          <h1>© Achiya Tzuriel</h1>
+        </div>
+        <div>
           <img
             src="https://gamearea.co.il/wp-content/uploads/2021/05/gamearea-web-14.png"
             alt=""
@@ -15,10 +18,31 @@ export const LoginButton = () => {
         </div>
         <button
           className="btn btn-primary w-100"
-          onClick={() => loginWithRedirect("http://localhost:3000")}
+          onClick={() => {
+            const redirectUrl =
+              window.location.hostname === "localhost"
+                ? "http://localhost:3000"
+                : "https://tic-tac-toe-of-achiya.netlify.app";
+            console.log(window.location.hostname);
+            loginWithRedirect({ redirectUri: redirectUrl });
+          }}
         >
           Log In
         </button>
+        {/* <button
+          className="btn btn-primary w-100"
+          onClick={() =>
+            loginWithRedirect("https://tic-tac-toe-of-achiya.netlify.app/")
+          }
+        >
+          Log In
+        </button>
+        <button
+          className="btn btn-primary w-100"
+          onClick={() => loginWithRedirect("http://localhost:3000")}
+        >
+          Log In
+        </button> */}
       </div>
     </div>
   );
